6aba4cca5deb15c074b4d5a3ef53cf6c11d78c33
Yuyuan <yuyuan.xiong@outlook.com>
2018-01-10 14:13:40 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 // Dom7
 var $$ = Dom7;
 
@@ -24,29 +25,127 @@ var app  = new Framework7({
   },
   // App routes
   routes: routes,
-  
+  init: false
+
 });
 
-app.on('pageInit', function (page) {
-  // do something on page init
-  alert('yoyyo')
-});
+var now = app.utils.now();
+//var query = app.utils.parseUrlQuery('http://google.com/?id=5&foo=bar');
+//console.log(query);
 
 // Init/Create main view
 var mainView = app.views.create('.view-main', {
   url: '/'
 });
-console.log(app.views.router.currentRoute.query);
-// Login Screen Demo
-$$('#my-login-screen .login-button').on('click', function () {
-  var username = $$('#my-login-screen [name="username"]').val();
-  var password = $$('#my-login-screen [name="password"]').val();
+var locationInfo = mainView.router.currentRoute.query;
 
-  // Close login screen
-  app.loginScreen.close('#my-login-screen');
 
-  // Alert username and password
-  app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
+
+$$(document).on('page:init', function (e) {
+
+  var page = e.detail;
+  var thisUrl = page.route.route.url
+  console.log(page.route.route.url);
+  var urlQuery = app.utils.parseUrlQuery(thisUrl);
+  var building= urlQuery.building;
+  var floors = urlQuery.floors;
+  var room= urlQuery.room;
+  alert(building);
+  alert(floors);
+  alert(room);
+
+  //user-form page
+
+  if (page.name === 'user-form') {
+    //star
+    $$('.stars a').click(function () {
+
+      $$('.stars i').text('star');
+
+      var starIndex = $$(this).attr('data-star');
+
+      for (var i = 0; i <= starIndex; i++) {
+        $$('.stars i').eq(i).text('star_fill');
+      }
+
+    });
+
+    //check phone num
+    $$('input[type="tel"]').on('keyup', function () {
+      var phoneNum = $$('#user-phone').val();
+
+      if (! phoneNum.match(/^-?\d+$/) && phoneNum !== '') {
+        $$('.user-reminder').text('请输入数字');
+      }else{
+        $$('.user-reminder').text('');
+      }
+    });
+
+    //check anonymous
+    // Create dynamic Popup
+    var dynamicPopup = app.popup.create({
+      content: '<div class="popup" id="user-popup">'+
+      '<div class="block">'+
+      '<i class="f7-icons">check_round</i><p>反馈提交成功！感谢您的支持.</p>'+
+      '</div>'+
+      '</div>',
+      // Events
+
+    });
+
+    $$('#user-submit').click(function () {
+      var username = $$('#username').val();
+      var userphone = $$('#user-phone').val();
+      if(username!=='' & userphone !== ''){
+
+        /* $$.ajax({
+          url:'',
+          data:{'data':jsonOrder},
+          type:'POST',
+          beforeSend:function(){
+            myApp.showPreloader('正在发送');
+          },
+          success:function(data)
+          {
+            myApp.hidePreloader();
+            dynamicPopup.open();
+            console.log(data);
+            if(data =='success')
+            {
+
+              alert('success');
+            }
+            else
+            {
+              alert('no data');
+            }
+
+          }
+        });*/
+
+
+
+
+      }else if(username!=='' & userphone === ''){
+        $$('.user-reminder').text('请输入电话号码');
+      }else if(username==='' & userphone === ''){
+        app.dialog.confirm('确定匿名提交？', '洗手间反馈调查', function () {
+          //ajax and popup page
+
+          dynamicPopup.open();
+
+        })
+      }
+    })
+
+
+
+
+  }
+
+
 });
+
+app.init()
 
 
